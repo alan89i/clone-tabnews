05bd6ee08485d4b09d2aabd340284bc0286841ef
alan89i <47751982+alan89i@users.noreply.github.com>
2025-04-13 22:08:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Elissandra libera o cuzinho ai &#x1F60B;</h1>
+function Home() {
+  return <h1>Elissandra libera o cuzinho ai &#x1F60B;</h1>;
 }
 
 export default Home;
